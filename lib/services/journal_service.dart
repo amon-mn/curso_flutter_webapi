2348adada00a513557030477225adebf9654bcfd
amonmn <84486422+amonmn@users.noreply.github.com>
2023-01-31 17:43:02 -0400

    Integrando WebAPI a um app mobile desenvolvido em Flutter.

---
+++ lib/services/journal_service.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+
+import 'package:flutter_webapi_first_course/models/journal.dart';
+import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_interceptor/http/http.dart';
+
+class JournalService {
+  static const String url = "http://10.0.0.113:3000/";
+  static const String resource = "journals/";
+
+  http.Client client =
+      InterceptedClient.build(interceptors: [LoggingInterceptor()]);
+
+  String getUrl() {
+    return "$url$resource";
+  }
+
+  Future<bool> register(Journal journal) async {
+    String jsonJournal = json.encode(journal.toMap());
+    http.Response response =
+        await client.post(
+            Uri.parse(getUrl()),
+            headers: {'Content-type': 'application/json'},
+            body: jsonJournal);
+
+    if(response.statusCode == 201){
+      return true;
+    }
+    return false;
+  }
+
+  Future<List<Journal>> getAll() async {
+    http.Response response = await client.get(Uri.parse(getUrl()));
+    if(response.statusCode != 200){
+      throw Exception();
+    }
+
+    List<Journal> journalsList = [];
+    List<dynamic> journalsDynamic = jsonDecode(response.body);
+
+    for(var jsonMap in journalsDynamic){
+      journalsList.add(Journal.fromMap(jsonMap));
+    }
+
+    return journalsList;
+  }
+}
